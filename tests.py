42727b71304ec8f35b53d0dce3b6f8713460e89f
Diego Berrueta <dberrueta@atlassian.com>
2020-09-15 09:53:53 +1000

    Use PyTest

--- tests.py
+++ tests.py
@@ -1,10 +1,2 @@
-import unittest
-
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_answer():
+    assert 0 == 1
