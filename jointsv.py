9d2e2b036d64cefdb467f20cff47613b1ba53079
Luis Lainez <llainez@atlassian.com>
2020-09-15 10:44:35 +1000

    Move methods first and implement as placeholders

--- jointsv.py
+++ jointsv.py
@@ -1,24 +1,10 @@
-
+import argparse
 from collections import defaultdict
 
-def main():
-    print("Starting Join SV")
-    filePath = "/" #read from stdin
-    output_file_path = "/oputput.txt" # get from stdin
-    output_file = open_file(output_file_path)
-    records = read_records_from_files()
-    output_list = []
-    for key,value in records.items():
-        output_list.append(process_record_list(value))
-    for item in sort_elements(output_file_path):
-        write_element(item, output_file)
-
-
-if __name__ == "__main__":
-    main()
 
 def open_file(output_file_path):
     #open file
+    print("TODO")
 
 def read_records_from_files(filePath):
     # ...
@@ -28,6 +14,7 @@ def process_record_list(record_list):
     # Create as many columns as samples
     # Process SVs if possible
     # if not possible return raw BNDs
+    print("TODO")
 
 def sort_elements(output_list):
     #sort the list before by start position
@@ -35,3 +22,20 @@ def sort_elements(output_list):
 
 def write_element(item, file):
     #write element in file
+    print("TODO")
+
+def main():
+    print("Starting Join SV")
+    filePath = "/" #read from stdin
+    output_file_path = "/oputput.txt" # get from stdin
+    output_file = open_file(output_file_path)
+    records = read_records_from_files(filePath)
+    output_list = []
+    for key,value in records.items():
+        output_list.append(process_record_list(value))
+    for item in sort_elements(output_file_path):
+        write_element(item, output_file)
+
+
+if __name__ == "__main__":
+    main()
