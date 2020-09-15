5b3c1e8a7eb00986700aadd7db84c8f45ae8a8fa
Luis Lainez <llainez@atlassian.com>
2020-09-15 13:24:41 +1000

    Check if file is directroy or not and get all names of files

--- file_reader.py
+++ file_reader.py
@@ -1,15 +1,37 @@
 from collections import defaultdict
+from os import listdir
+from os.path import isfile, join
 
 
 def read_records_from_files(file_path_list, chromosome_list):
     multi_map = defaultdict(list)
-    for file_path in file_path_list:
-        data_started = False
-        file = open(file_path, 'r')
-        while not data_started:
-            line = file.readline()
-            if line.startswith("#CHROM"):
-                data_started = True
-                
+    for file_path in collect_all_file_names(file_path_list):
+        print(file_path)
+        # reader = vcfpy.Reader.from_path(file_path)
+        # for record in reader:
+        #     if not record.is_snv():
+        #         continue
+        #     line = [record.CHROM, record.POS, record.ID, record.REF]
+        #     line += [record.ALT]
+        #     line += [call.data.get('GT') or './.' for call in record.calls]
+        #     print('\t'.join(map(str, line)))
 
     return multi_map
+
+
+def collect_all_file_names(file_path_list):
+    files = []
+    for file_path in file_path_list:
+        if is_file_path_a_directory(file_path):
+            files.extend(read_all_file_names_from_directory(file_path))
+        else:
+            files.append(file_path)
+    return files
+
+
+def is_file_path_a_directory(file_path):
+    return file_path.endswith('/')
+
+
+def read_all_file_names_from_directory(directory_path):
+    return [directory_path + f for f in listdir(directory_path) if isfile(join(directory_path, f))]
