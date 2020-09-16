c30ac68cd8919f485708e820641e9c5a97a5d053
Diego Berrueta <dberrueta@atlassian.com>
2020-09-17 08:52:22 +1000

    Do not fail parsing when ALT does not contain an end position

--- file_reader.py
+++ file_reader.py
@@ -36,7 +36,7 @@ def read_records_from_files(file_path_list, chromosome_set):
                 # position is not necessarily the one in the input BND, but it could be the end position if
                 # it references a previous position.
                 min_pos = get_start_position(record)
-                if not is_record_an_sv(record):
+                if get_end_position(record):
                     min_pos = min(get_start_position(record), get_end_position(record))
                 record_location = (record.CHROM, min_pos)
 
--- sv_detector.py
+++ sv_detector.py
@@ -3,7 +3,7 @@ from record_helper import *
 
 # Maybe name doesnt match the logic, but it's what we found so far
 def is_record_an_sv(record):
-    return isinstance(record.ALT, vcfpy.BreakEnd)
+    return isinstance(record.ALT[0], vcfpy.BreakEnd)
 
 
 def are_pair_records(record1, record2):
