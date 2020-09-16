c8f84aa3b88d91e9498a808e5b4d130583c40d89
Diego Berrueta <dberrueta@atlassian.com>
2020-09-16 10:57:07 +1000

    Patch the problem around the type of the fields in the call column

--- jointsv.py
+++ jointsv.py
@@ -59,9 +59,9 @@ def get_sample_call(sample_name, original_record):
     # We may not have data in this group for all samples, so we fill the blanks with default values
     call_data.setdefault(".")
     if original_record:
-        call_data["GT"] = "42" # TODO: how to calculate this?
-        call_data["TRANCHE2"] = original_record.INFO["TRANCHE2"]
-        call_data["VAF"] = original_record.INFO["BNDVAF"]
+        call_data["GT"] = "0" # TODO: how to calculate this?
+        call_data["TRANCHE2"] = [original_record.INFO["TRANCHE2"]]
+        call_data["VAF"] = [str(original_record.INFO["BNDVAF"])] # TODO: remove the string conversion when we declare VAF as a float
 
     return vcfpy.Call(sample=sample_name, data=call_data)
 
