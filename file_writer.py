b4f808d606e6616de9e05946b45d71766ee955f3
Diego Berrueta <dberrueta@atlassian.com>
2020-09-16 14:34:27 +1000

    Extract Python file

---
+++ file_writer.py
@@ -0,0 +1,59 @@
+import vcfpy
+
+
+def write_output(output_list, output_file_path, sample_names_to_header):
+    """
+    Serialises the data into a VCF file.
+
+    :param output_list:
+    :param output_file_path:
+    :param sample_names_to_header:
+    :return:
+    """
+    assert len(sample_names_to_header) > 0, "At least one sample is required"
+
+    # Sort the output by chromosome and position
+    sorting_function = lambda record: (record.CHROM, record.POS)
+    output_list.sort(key=sorting_function)
+
+    header = get_header(sample_names_to_header.keys())
+
+    writer = vcfpy.Writer.from_path(output_file_path, header)
+
+    for output_record in output_list:
+        writer.write_record(output_record)
+
+    writer.close()
+
+
+def get_header(sample_names):
+    header = vcfpy.Header()
+
+    # INFO fields
+    header.add_info_line(vcfpy.OrderedDict(
+        ID="END",
+        Number=1,
+        Type="Integer",
+        Description="Stop position of the interval"))
+
+    # FORMAT fields
+    header.add_format_line(vcfpy.OrderedDict(
+        ID="GT",
+        Number=1,
+        Type="String",
+        Description="Genotype"))
+    header.add_format_line(vcfpy.OrderedDict(
+        ID="TRANCHE2",
+        Number=1,
+        Type="String",
+        Description="Quality category of GRIDSS structural variant calls determined using FILTER,SRQ,AS,RAS. Values are LOW INTERMEDIATE HIGH"))
+    header.add_format_line(vcfpy.OrderedDict(
+        ID="VAF",
+        Number=1,
+        Type="Float",
+        Description="VAF of this SV call, derived from BNDVAF values of BND calls used to call this SV"))
+
+    # Samples
+    header.samples = vcfpy.SamplesInfos(sample_names)
+
+    return header
--- jointsv.py
+++ jointsv.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 
 from argument_parser import parse_arguments
 from file_reader import read_records_from_files
+from file_writer import write_output
 from record_helper import *
 import logging
 
@@ -153,64 +154,6 @@ def generate_non_sv_records(colocated_records, sample_names):
             calls=calls))
 
     return output
-
-
-def write_output(output_list, output_file_path, sample_names_to_header):
-    """
-    Serialises the data into a VCF file.
-
-    :param output_list:
-    :param output_file_path:
-    :param sample_names_to_header:
-    :return:
-    """
-    assert len(sample_names_to_header) > 0, "At least one sample is required"
-
-    # Sort the output by chromosome and position
-    sorting_function = lambda record: (record.CHROM, record.POS)
-    output_list.sort(key=sorting_function)
-
-    header = get_header(sample_names_to_header.keys())
-
-    writer = vcfpy.Writer.from_path(output_file_path, header)
-
-    for output_record in output_list:
-        writer.write_record(output_record)
-
-    writer.close()
-
-
-def get_header(sample_names):
-    header = vcfpy.Header()
-
-    # INFO fields
-    header.add_info_line(vcfpy.OrderedDict(
-        ID="END",
-        Number=1,
-        Type="Integer",
-        Description="Stop position of the interval"))
-
-    # FORMAT fields
-    header.add_format_line(vcfpy.OrderedDict(
-        ID="GT",
-        Number=1,
-        Type="String",
-        Description="Genotype"))
-    header.add_format_line(vcfpy.OrderedDict(
-        ID="TRANCHE2",
-        Number=1,
-        Type="String",
-        Description="Quality category of GRIDSS structural variant calls determined using FILTER,SRQ,AS,RAS. Values are LOW INTERMEDIATE HIGH"))
-    header.add_format_line(vcfpy.OrderedDict(
-        ID="VAF",
-        Number=1,
-        Type="Float",
-        Description="VAF of this SV call, derived from BNDVAF values of BND calls used to call this SV"))
-
-    # Samples
-    header.samples = vcfpy.SamplesInfos(sample_names)
-
-    return header
 
 
 def main(args):
