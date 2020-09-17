e5dd9cf8a89e193d5f2dd8d558b29c07d64465e4
Peter Grasevski <pgrasevski@atlassian.com>
2020-09-17 15:36:47 +1000

    Test todos

--- tests.py
+++ tests.py
@@ -4,6 +4,8 @@ import unittest
 
 class TestJointSv(unittest.TestCase):
 
+    # TODO We should refactor jointsv to allow us to output to stdout so that we don't need to create output test files
+    # TODO We should provide more meaningful test failure output, for example a diff between the files
     def test_simple(self):
         self.assertEqual(1, 1, 'Should be 1')
         subprocess.run(['mkdir', 'testoutput'])
@@ -15,6 +17,8 @@ class TestJointSv(unittest.TestCase):
         subprocess.run(['rm', '-rf', 'testoutput'])
         self.assertEqual(output, expected_output)
 
+    # TODO We should add lower level tests that assert for a given input, we expect certain SVs to be called and
+    #  exist in the output instead of just asserting against a whole output file
 
 if __name__ == '__main__':
     unittest.main()
